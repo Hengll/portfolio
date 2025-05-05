786d0ab454db37f471a8a2d9ac896ac9a5fd127f
HengLi <heeric891120@gmail.com>
2025-05-06 02:54:17 +0800

    2025/5/6 新增loading動畫(文字) section改成元件架構

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,65 @@
 <template>
-  <div
-    class="loading-page absolute w-full h-dvh bg-gray-300 flex flex-col justify-center items-center"
-  >
+  <div class="loading-page absolute w-full h-dvh flex flex-col justify-center items-center">
     <DotLottieVue
       style="height: 200px; width: 200px"
       autoplay
       loop
       src="/Animation-1743077041364.lottie"
     />
-    <p class="name text-5xl mt-5">Heng Li</p>
+    <p class="name select-none text-5xl mt-15">Heng Li</p>
   </div>
   <div v-if="!isLoading" class="contect-page">
-    <div class="first-view container mx-auto px-10 h-dvh bg-amber-300"></div>
-    <div class="about container mx-auto px-10 h-dvh bg-amber-500"></div>
-    <div class="portfolio container mx-auto px-10 h-dvh bg-amber-700"></div>
-    <div class="contact container mx-auto px-10 h-dvh bg-amber-900"></div>
+    <FirstView class="first-view container mx-auto px-10 h-dvh bg-amber-300" />
+    <AboutMe class="about-me container mx-auto px-10 h-dvh bg-amber-500" />
+    <MyPortfolio class="my-portfolio container mx-auto px-10 h-dvh bg-amber-700" />
+    <ContactMe class="contact-me container mx-auto px-10 h-dvh bg-amber-900" />
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { DotLottieVue } from '@lottiefiles/dotlottie-vue' // 沙漏動畫套件
+import { gsap } from 'gsap'
+import { SplitText } from 'gsap/SplitText'
+
+import FirstView from './components/FirstView.vue'
+import AboutMe from './components/AboutMe.vue'
+import MyPortfolio from './components/MyPortfolio.vue'
+import ContactMe from './components/ContactMe.vue'
+
+gsap.registerPlugin(SplitText)
 
 let isLoading = ref(true)
+let splitName
+let timeLine
 
-setTimeout(() => {
-  isLoading.value = false
-}, 4000)
+onMounted(() => {
+  splitName = SplitText.create('.name', { type: 'words, chars' })
+
+  timeLine = gsap.timeline()
+  timeLine.fromTo(
+    splitName.chars,
+    { y: 20 },
+    {
+      y: -20,
+      duration: 0.38,
+      ease: 'sine.inOut',
+      stagger: {
+        each: 0.1,
+        repeat: 1,
+        yoyo: true,
+      },
+      repeat: 2,
+      delay: 0.1,
+      onComplete: () => {
+        isLoading.value = false
+
+        // gsap.to('.loading-page', {
+        //   alpha: 0,
+        //   direction: 1,
+        // })
+      },
+    },
+  )
+})
 </script>
